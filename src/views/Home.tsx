c76a46057f3e949518f6107a0d179c1eac3dcb73
Parker Gates <parkergates14@gmail.com>
2023-08-15 02:13:32 -0600

    sections added to home, header as logo, grids and snakes added

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import Grid1 from '../svgs/grid1.svg';
+import Grid2 from '../svgs/grid2.svg';
+import AspLogo from '../svgs/AspLogoLarge.svg';
+import GreenSnake from '../images/greenSnake.png';
+
+import './css/Home.css';
 
 export default function Home() {
 
     return(
         <div>
-            <h1>Home</h1>
+            <div className='hm_heading'>
+                <img src={AspLogo} className='hm_heading_logo' alt="asp logo" />
+                <img src={Grid1} className='hm_heading_grid1' alt="grid" />
+                <img src={Grid2} className='hm_heading_grid2' alt="grid" />
+                <img src={GreenSnake} className="hm_heading_snake" alt="green snake" />
+            </div>
+
+
+
+
+
+            <div className='hm_call2action'>
+
+            </div>
+            <div className='hm_options'>
+
+            </div>
+            <div className="hm_popular">
+
+            </div>
+            <div className='hm_testimonials'>
+
+            </div>
+            <div className="hm_sales">
+
+            </div>
+            <footer className='hm_footer'>
+
+            </footer>
         </div>
     );
 }
