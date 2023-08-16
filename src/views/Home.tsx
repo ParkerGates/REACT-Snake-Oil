abbd4d1365501828484f739472af351e32a9e77e
Parker Gates <parkergates14@gmail.com>
2023-08-16 15:49:22 -0600

    updating main branch overlap

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -6,6 +6,11 @@ import AspLogo from '../svgs/AspLogoLarge.svg';
 import GreenSnake from '../images/greenSnake.png';
 import HomeEllipseLeft from '../svgs/HomeEllipseLeft.svg';
 import HomeEllipseRight from '../svgs/HomeEllipseRight.svg';
+import HandshakeOutlinedIcon from '@mui/icons-material/HandshakeOutlined';
+import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined';
+import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
+
+
 
 import './css/Home.css';
 
@@ -46,11 +51,27 @@ export default function Home() {
                 <div className="hm_call2action_divider2"></div>
                 <img className="hm_ellipse_1" src={HomeEllipseLeft} alt="ellipse" />
                 <img className="hm_ellipse_2" src={HomeEllipseRight} alt="ellipse" />
-
             </div>
+
             <div className='hm_options'>
+                <div>
+                    <span className='hm_option_icon_cont'><HandshakeOutlinedIcon /></span>
+                    <NavLink className='hm_options_btn' to="/about">About</NavLink>
+                </div>
+                <div>
+                    <span className='hm_option_icon_cont'><InfoOutlinedIcon/></span>
+                    <a className='hm_options_btn' href="">Testimonials</a>
+                </div>
+                <div>
+                    <span className='hm_option_icon_cont'><AttachMoneyOutlinedIcon/></span>
+                    <a className='hm_options_btn' href="">Sales</a>
+                </div>
+
+
+
 
             </div>
+
             <div className="hm_popular">
 
             </div>
