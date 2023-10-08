61d4011c40db2f1e89466c9993b81e7351333d97
Parker Gates <parkergates14@gmail.com>
2023-10-08 15:45:08 -0600

    added multi click protection to add to cart buttons

--- src/components/DisplayItemTile/DisplayItemTile.tsx
+++ src/components/DisplayItemTile/DisplayItemTile.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { StoreItem } from '../../interface/interfaces';
 import { Link, useNavigate } from 'react-router-dom';
 import { useContextData } from '../../context/context';
 import Stars from '../Stars/Stars';
@@ -12,20 +13,36 @@ interface Props {
 export default function DisplayItemTile({item}: Props) {
     const navigate = useNavigate();
     const { appData, dispatch } = useContextData(); 
+    const [lastAdded, setLastAdded] = useState<StoreItem|undefined>(undefined);
+
+    const addToCart = (storeItem: StoreItem) => {
+        dispatch({type:'addToCart',storeItem:storeItem});
+        setLastAdded(storeItem);
+
+    }
+
+    useEffect(() => {
+        if (lastAdded !== undefined) {
+            const addCartTimout = setTimeout(() => {
+                setLastAdded(undefined);
+            }, 1500);
+        }
+    },[lastAdded]);
+
 
     return (
         <div className='shp_tile' key={item.name}>
             <div className='shp_tile_img_cont'>
                 <img className="shp_tile_img" src={item.image} alt={item.name} />
                 <div className='shp_tile_interaction' onClick={(e)=>{e.target === e.currentTarget && navigate('/shop/'+item.alias)}}>
-                    <button className="shp_tile_interaction_cart" onClick={()=>{dispatch({type:'addToCart',storeItem:item})}}>
-                        <AddShoppingCartIcon fontSize='large' sx={{fontSize:'4.2rem'}}/>
-                    </button>
-                    <Link to={'/shop/'+ item.alias}><button className="shp_tile_interaction_details">Details</button></Link>
-                    <div className='shp_tile_interaction_test'>
+                    {   lastAdded?.alias !== item.alias ?
+                        <button className="shp_tile_interaction_cart" onClick={()=>{addToCart(item)}}>
+                                <AddShoppingCartIcon fontSize='large' sx={{fontSize:'4.2rem'}}/>
+                        </button>
+                        :
                         <div className='shp_tile_added_title'>Added</div>
-                        <button className='shp_tile_added_cancel'>Cancel</button>
-                    </div>
+                    }
+                    <Link to={'/shop/'+ item.alias}><button className="shp_tile_interaction_details">Details</button></Link>
                 </div>
             </div>
             <div onClick={()=>{navigate('/shop/'+item.alias)}}>
