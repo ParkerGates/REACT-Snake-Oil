3d4b32ca152da16105bc4e2f74df7c4c6895893c
Parker Gates <parkergates14@gmail.com>
2023-11-20 01:12:17 -0700

    secret snake page added

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar';
 import Home from './views/Home';
 import About from './views/About';
 import Shop from './views/Shop';
+import Snake from './views/Snake';
 import './App.css';
 
 export default function App() {
@@ -17,6 +18,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/shop/:details?" element={<Shop />} />
+        <Route path='/snake' element={<Snake />} />
       </Routes>
     </BrowserRouter>
     </div>
