1301ed9d51bf3172dc75a1dad7932149eb1be52f
Parker Gates <parkergates14@gmail.com>
2023-11-19 23:22:00 -0700

    popup appears if json api is not currently running

--- src/context/context.tsx
+++ src/context/context.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useReducer, useState, useEffect } from "react";
+import ApiCheckPopup from "../components/ApiCheckPopup/ApiCheckPopup";
 import axios from 'axios';
 import { initialState, reducer } from './reducer';
 
@@ -12,13 +13,16 @@ export function useContextData() { return useContext(ContextData)}
 export default function AppContext({children}: Props) {
     const [appData, dispatch] = useReducer(reducer, initialState);
     const [screenW, setScreenW] = useState<number>(window.innerWidth);
+    const [hasApi, setHasApi] = useState<undefined|boolean>(undefined);
 
     const updateScreenChange = () => { setScreenW(window.innerWidth) }
 
     useEffect(()=> {
         axios.get('http://localhost:4000/data').then((res)=>{
+            setHasApi(true);
             dispatch({type:'mountStoreData', newStoreData: res.data})
         }).catch((error)=>{
+            setHasApi(false)
             console.log('Server not reached. Please run `npm run serve-json` in another terminal then reload page', error)
         });
 
@@ -30,7 +34,10 @@ export default function AppContext({children}: Props) {
 
     return (
         <ContextData.Provider value={{appData, dispatch, screenW}}>
+            <>
+            <ApiCheckPopup hasApi={hasApi}/>
             {children}
+            </>
         </ContextData.Provider>
     );
 }
